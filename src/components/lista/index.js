d73c78d06cd7aab63dc7b6fd3981187a79724f4e
Mario Leandro <Mario Leandro>
2018-12-22 14:44:10 -0200

    pegando o caminho certo

---
+++ src/components/lista/index.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Lista extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+    }
+
+    Render(){
+        return (
+            
+        );
+    };
+};
+
+export default Lista;
--- src/pages/pdv/index.js
+++ src/pages/pdv/index.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import {Col, Button} from 'reactstrap';
 import Autocomplete from '../../components/Autocomplete';
 
@@ -7,24 +7,37 @@ import 'react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
 import './styles.css';
 
 class TelaPdv extends Component {
+
     constructor(props) {
         super(props);
 
         this.state = {
-            produtos: [
-                {id:1,descr:'pulseira',preco: 20.00},
-                {id:2,descr:'anel',preco: 15.35},
-                {id:3,descr:'boné',preco: 16.50},
-                {id:4,descr:'camisa',preco: 19.00}
-            ],
+            produtos: [],
             produtoAtual: [],
             itensVendidos: []
         };
     }
 
+    componentDidMount(){
+        const produtos = [
+            {id:1,descr:'pulseira',preco: 20.00},
+            {id:2,descr:'anel',preco: 15.35},
+            {id:3,descr:'boné',preco: 16.50},
+            {id:4,descr:'camisa',preco: 19.00}
+        ];
+    
+        this.setState({
+            produtos: produtos
+        });
+    }
+
     render(){
         const decimais = (numero) => numero.toFixed(2).replace(".",",");
 
+        const mudaSubtotal = () => {
+            document.querySelector('#preco-subtotal').value = decimais(this.state.produtoAtual[0].preco * document.getElementById('quant').value);
+        };
+        
         const produtoGet = (texto) => {
             this.setState({produtoAtual: this.state.produtos.filter(produto => produto.descr === texto)});
             if(this.state.produtoAtual.length){
@@ -33,25 +46,42 @@ class TelaPdv extends Component {
             }
         };
 
-        const mudaSubtotal = () => {
-            document.querySelector('#preco-subtotal').value = decimais(this.state.produtoAtual[0].preco * document.getElementById('quant').value);
+        const totalGeral = () => {
+            if(this.state.itensVendidos.length){ 
+                return(
+                    decimais(
+                        this.state.itensVendidos
+                            .map(item => item.subTotal)
+                            .reduce((a, s)=>a + s)
+                    )
+                );
+            }
         };
 
         const addProd = () => {
-            let quant = document.getElementById('quant').value;
+            let itensVendidos = [];
+
+            itensVendidos = Object.assign(itensVendidos,this.state.itensVendidos);
+
             let item = {
                 id: this.state.itensVendidos.length + 1,
                 descr: this.state.produtoAtual[0].descr,
-                quant: quant,
+                quant: document.getElementById('quant').value,
                 unit: this.state.produtoAtual[0].preco,
-                total: this.state.produtoAtual[0].preco * quant
+                subTotal: this.state.produtoAtual[0].preco * document.getElementById('quant').value
             };
-            this.state.itensVendidos.push(item);
+            
+            itensVendidos.push(item);
+
+            this.setState({
+                itensVendidos: itensVendidos
+            });
+
             document.querySelector('.choose-produto input').value="";
             document.querySelector('#preco').value ="0,00";
             document.querySelector('#preco-subtotal').value ="0,00";
             document.querySelector('#quant').value ="1";
-            document.querySelector('#total').value ="0,00";
+            document.querySelector('#total').value = totalGeral();
         };
 
         return (
@@ -69,7 +99,16 @@ class TelaPdv extends Component {
                 </Col>
 
                 <Col md={6} className={`itens-vendidos`}>
-                    <div className='lista-itens'>Lista de ítens</div>
+                    <div className='lista-itens'>
+                        
+                            {this.state.itensVendidos.map(item => (
+                                <Fragment>
+                                <Col md={6} className="lista-itens__ul__li" key={item.id}>{item.descr}</Col>
+                                <Col md={6} className="lista-itens__ul__li">{item.unit}</Col>
+                                </Fragment>
+                            ))}
+                        
+                    </div>
                     <div className='total'><span className='total__legenda'>Total:</span><input className='form-control total__input' type='text' id='total' defaultValue='0,00' /></div>
                     <div className='btn-pagar'><Button color='success' className="form-control">Pagar Agora</Button></div>
                 </Col>
